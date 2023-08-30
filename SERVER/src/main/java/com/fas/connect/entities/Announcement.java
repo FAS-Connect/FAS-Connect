ad0da108814826a7e79cda1af9dec6a20dc83e92
Aditi <aditiagrawal10000@gmail.com>
2023-08-30 16:12:42 +0530

    Added:Announcement Related Entities, Repo, Service, Controller and DTOs

---
+++ SERVER/src/main/java/com/fas/connect/entities/Announcement.java
@@ -0,0 +1,24 @@
+package com.fas.connect.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity  
+@Table(name="announcement")  
+@PrimaryKeyJoinColumn(name="id") 
+public class Announcement extends Post {
+	
+}
